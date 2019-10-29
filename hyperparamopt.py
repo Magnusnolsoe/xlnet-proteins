aa350019b6a86e725e6ce55ae09bc690da75b471
FrederikWA <FrederikWA@users.noreply.github.com>
2019-10-29 17:51:26 +0100

    Hyper parameters defined

--- hyperparamopt.py
+++ hyperparamopt.py
@@ -1,5 +1,6 @@
 from sigopt import Connection
 from absl import flags, app
+import numpy as np  
 
 # SigOpt parameters
 flags.DEFINE_string("name", default="",
@@ -13,7 +14,14 @@ flags.DEFINE_integer("num_workers", default=0,
 flags.DEFINE_string("api_token", default="",
       help="SigOpt api token")
 
+# Experiment parameters
+flags.DEFINE_integer("seq_len", default=0,
+      help="Seq len of dataset")
+
+MAX_WARMUP_STEPS = 5000
+
 def spin_up_worker(experiment_id):
+    
     # TODO: Create folders, config files and start worker
     pass
 
@@ -26,7 +34,20 @@ def master(_):
         project=FLAGS.project_id,
         metrics=[dict(name='pplx', objective='minimize')],
         parameters=[
-            dict(name='', type='', bounds=dict(min=,max=)),
+            dict(name='mem_len', type='int', bounds=dict(min=0,max=500)),
+            dict(name='perm_size', type='int', bounds=dict(min=1,max=FLAGS.seq_len/2)),
+            dict(name='n_layer', type='int', bounds=dict(min=1,max=24)),
+            dict(name='d_model', type='int', bounds=dict(min=1,max=1024)),
+            dict(name='d_embed', type='int', bounds=dict(min=1,max=1024)),
+            dict(name='n_head', type='int', bounds=dict(min=1,max=16)),
+            dict(name='d_head', type='int', bounds=dict(min=1,max=64)),
+            dict(name='d_inner', type='int', bounds=dict(min=1,max=4096)),
+            dict(name='batch_size', type='categorical', categorical_values=[64, 32, 16, 8]),
+            dict(name='learning_rate', type='double', bounds=dict(min=1e-6, max=1e-1)),
+            dict(name='dropout', type='double', bounds=dict(min=0,max=1)),
+            dict(name='dropatt', type='double', bounds=dict(min=0,max=1)),
+            dict(name='warmup_steps', type='int', bounds=dict(min=0,max=MAX_WARMUP_STEPS)),
+            dict(name='weight_decay', type='categorical', categorical_values=[1.e-08, 1.e-07, 1.e-06, 1.e-05, 1.e-04, 1.e-03, 1.e-02, 1.e-01, 1.e+00, 1.e+01])
         ],
         metadata=dict(
             'avg_training_time'=0,
