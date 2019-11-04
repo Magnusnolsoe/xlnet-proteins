5cc51562e4892f56b669d3c7fb84730caadfff2c
Magnus Nolsøe <magnusnolsoe@gmail.com>
2019-11-04 17:24:35 +0100

    json added to train_tpu.py

--- train_tpu.py
+++ train_tpu.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
+import os, json
 
 from os_utils import get_logdir
 from absl import app
