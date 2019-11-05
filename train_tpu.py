6ead73c016693d26f281a777ed64cfd15e81ea25
Magnus Nolsøe <s144471@win.dtu.dk>
2019-11-05 17:13:42 +0100

    temp

--- train_tpu.py
+++ train_tpu.py
@@ -373,7 +373,7 @@ def main(unused_argv):
         'avg_eval_time': str(np.mean(eval_times)),
         'stopped_early': str(stopped_early),
   }
-  with tf.gfile.Open(os.path.join(FLAGS.bucket_uri, "results", FLAGS.run_id), "w") as fp:
+  with tf.gfile.Open(os.path.join(FLAGS.bucket_uri, "results", "{}.json".format(FLAGS.run_id)), "w") as fp:
         json.dump(result, fp)
 
 
