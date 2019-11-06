fa9cad6e424af52c39eebed29b6a119d69201f33
Magnus Nolsøe <s144471@win.dtu.dk>
2019-11-06 14:20:30 +0100

    minor mistake

--- train_tpu.py
+++ train_tpu.py
@@ -208,8 +208,7 @@ def get_model_fn(logdir):
       '''
       #### Constucting training TPUEstimatorSpec with new cache.
       train_spec = tf.contrib.tpu.TPUEstimatorSpec(
-            mode=mode, loss=total_loss, train_op=train_op, '''host_call=host_call,'''
-            scaffold_fn=scaffold_fn)
+            mode=mode, loss=total_loss, train_op=train_op, scaffold_fn=scaffold_fn) '''host_call=host_call,'''
       train_spec.cache = new_cache
 
       return train_spec
