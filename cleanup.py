e1533d0e0e348c715b7ecedfd69fdfc3da69d646
Magnus Nolsøe <s144471@win.dtu.dk>
2019-11-06 16:11:58 +0100

    now possible to delete open suggestions in cleanup

--- cleanup.py
+++ cleanup.py
@@ -9,6 +9,8 @@ flags.DEFINE_string("experiment_id", default="default",
       help="Experiment id")
 flags.DEFINE_string("bucket_name", default="",
         help="Name of gcp bucket")
+flags.DEFINE_bool("total", default=False,
+        help="Total cleanup or not")
 
 def main(_):
     
@@ -26,6 +28,17 @@ def main(_):
         tf.gfile.Remove(param_config_file)
 
     conn.experiments(FLAGS.experiment_id).observations().delete(state="failed")
+
+    if FLAGS.total:
+        suggestions = conn.experiments(FLAGS.experiment_id).suggestions().fetch(state="open")
+        for sugg in suggestions.iterate_pages():
+
+            model_dir = os.path.join(FLAGS.bucket_name, "models", suggestion_id)
+            param_config_file = os.path.join(FLAGS.bucket_name, "param_configs", "{}.json".format(suggestion_id))
+            tf.gfile.DeleteRecursively(model_dir)
+            tf.gfile.Remove(param_config_file)
+        
+        conn.experiments(FLAGS.experiment_id).suggestions().delete(state="open")
 
 
 if __name__ == '__main__':
