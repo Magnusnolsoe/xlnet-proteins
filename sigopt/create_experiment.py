4eb7bf9133d68979445abba7435cad8d8f8f8329
Magnus Nolsøe <s144471@win.dtu.dk>
2020-01-14 17:31:30 +0100

    minor changes

--- sigopt/create_experiment.py
+++ sigopt/create_experiment.py
@@ -22,7 +22,7 @@ def main(unused_args):
     experiment = conn.experiments().create(
         name=FLAGS.exp_name,
         project=FLAGS.project_id,
-        metrics=[dict(name='pplx', objective='minimize')],
+        metrics=[dict(name="acc", objective='maximize')],
         parameters=[
                 #dict(name='mem_len', type='int', bounds=dict(min=0,max=125)),        # Multiply
                 #dict(name='perm_size', type='int', bounds=dict(min=1,max=2)),       # Function
@@ -33,9 +33,9 @@ def main(unused_args):
                 #dict(name='d_head', type='int', bounds=dict(min=1,max=6)),          # Multiply
                 #dict(name='d_inner', type='int', bounds=dict(min=6,max=11)),        # Multiply
                 #dict(name='seq_len', type='categorical', categorical_values=['32', '64', '128', '256', '512']),
-                dict(name='learning_rate', type='double', bounds=dict(min=1e-10, max=1e-4)), # Multiply
-                dict(name='decay_method', type='categorical', categorical_values=['poly', 'cos']),
-                dict(name='dropout', type='int', bounds=dict(min=0,max=5)),         # Multiply
+                dict(name='learning_rate', type='double', bounds=dict(min=1e-7, max=1e-4)), # Multiply
+                #dict(name='decay_method', type='categorical', categorical_values=['poly', 'cos']),
+                dict(name='dropout', type='int', bounds=dict(min=0,max=4)),         # Multiply
                 dict(name='dropatt', type='int', bounds=dict(min=0,max=5)),         # Multiply
                 dict(name='warmup_steps', type='int', bounds=dict(min=0,max=10)),   # Multiply
                 dict(name='lr_layer_decay_rate', type='int', bounds=dict(min=0, max=10)),  # Mutliply
